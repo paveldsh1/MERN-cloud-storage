8c707a77a4439607656a12b0d53de867b2287762
PavelDenisov1 <paveldenisovqwe01@mail.ru>
2026-09-25 10:18:37 +0400

    added cors on server side

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import { getAuthRouter, loginRouter } from './routes/authRouter.js'
 import { getRootRoute } from './routes/rootRouter.js'
+import { cors } from './middleware/corsMiddleware.js'
 
 export const app = express()
 
 app.use(express.json())
+app.use(cors)
 
 app.use('/api/auth', getAuthRouter(), loginRouter())
 app.use('/', getRootRoute())
